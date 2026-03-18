8eeb560ce0fa9e0ff7022a6ec15bb1ba8c586ca4
ramdhivakar <ramdhivakar.s@gmail.com>
2026-03-18 09:47:59 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import {Button} from "@/components/ui/button";
 
 const Page = () => {
   return (
     <div>
-      <h1 className="text-2xl underline"></h1>
+      <h1 className="text-2xl underline">Welcome to my SaaS App</h1>
+        <Button> Let's get started!</Button>
     </div>
   )
 }
